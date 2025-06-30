e08ada824cffa636ede39722f91a85446ddefab6
jv.macbook <44486596+willjandal@users.noreply.github.com>
2025-06-30 21:44:11 +0400

    installed playwright

---
+++ tests/example.spec.js
@@ -0,0 +1,22 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('Navigate to Homepage.', async ({ page }) => {
+  await page.goto('/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/JV William | QA Engineer | Web App Testing Specialist/);
+});
+
+test('Navigate and check Experience section contents.', async ({ page }) => {
+  await page.goto('/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Experience' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Experience' })).toBeVisible();
+
+  // Expects page to have the role Test Enginner.
+  await expect(page.getByRole('heading', { name: 'Test Engineer · Web' })).toBeVisible();
+});
