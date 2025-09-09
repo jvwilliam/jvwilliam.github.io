a29b56bdd10d9aad00780d81f710ca33c6de5628
jv.macbook <44486596+willjandal@users.noreply.github.com>
2025-09-09 12:48:13 +0400

    added cross-env and refactored config file

--- playwright.config.js
+++ playwright.config.js
@@ -5,46 +5,28 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-require('dotenv').config({path: '.env.local'});
+import dotenv from 'dotenv';
+import path from 'path';
 
-const {BASE_URL}  = process.env;
-
-const config = {
-  use: {
-    baseURL: BASE_URL,
-  },
-};
-
-module.exports = config;
-
-
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+const ENV = process.env.ENV || 'local';
+dotenv.config({ path: path.resolve(__dirname, `.env.${ENV}`)});
 
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  testDir: './e2e',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  testDir: './tests',
+  fullyParallel: true, /* Run tests in files in parallel */
+  forbidOnly: !!process.env.CI, /* Fail the build on CI if you accidentally left test.only in the source code. */
+  retries: process.env.CI ? 2 : 0, /* Retry on CI only */
+  workers: process.env.CI ? 1 : undefined, /* Opt out of parallel tests on CI. */
+  reporter: 'html',   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: BASE_URL,
+    baseURL: process.env.BASE_URL, /* Base URL to use in actions like `await page.goto('/')`. */
     headless: true,
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on-first-retry', /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
   },
 
   /* Configure projects for major browsers */
